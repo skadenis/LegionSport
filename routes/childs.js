c380b5ab876cfb4460deaf1d2a5f63fb554c5b48
Denis <skadenis@mail.ru>
2020-10-25 22:00:19 +0300

    add objects

--- routes/childs.js
+++ routes/childs.js
@@ -147,9 +147,9 @@ router.post('/all_child_in_group', Policy(), verifyToken, CheckAuthorization, Wa
 router.post('/edit', Policy(), verifyToken, CheckAuthorization, ManageRights, async function(req, res, next) {
     switch (await new Schema(await RequestFormat.edit_child()).validate(req.body)) {
         case true:
-console.log(req.body.id);
+            let id = req.body.id;
+
             if(req.body.id === 0){
-                console.log(req.body.id);
                 delete req.body.id;
                 let data = await childs.create_child(req.body);
                 switch (data.status) {
@@ -173,7 +173,6 @@ console.log(req.body.id);
                         break
                 }
             }else{
-                let id = req.body.id;
                 let data = await childs.edit_child(req.body);
                 switch (data.status) {
                     case 200:
