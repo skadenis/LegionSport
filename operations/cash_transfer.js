8bc688f527dca568b655738caa6d7c5f94a814a2
Denis <skadenis@mail.ru>
2020-10-25 16:37:09 +0300

    fix childs

--- operations/cash_transfer.js
+++ operations/cash_transfer.js
@@ -1,7 +1,7 @@
 'use strict';
 let DataBase = require('../components/database/index');
 
-let childs = require('./childs');
+let childs_module = require('./childs');
 
 module.exports = class cash_transfer {
     constructor(){
@@ -15,11 +15,11 @@ module.exports = class cash_transfer {
         // });
     }
     async get_child_payments(data){
-        let info = await new childs().get_child_info({id:data.id});
+        let info = await new childs_module().get_child_info({id:data.id});
         let r_data;
          switch (info.status) {
              case 200:
-                 r_data = {status: 200, data:await new DataBase('cash_transfer').getBy('child_id', data.id)};
+                 r_data = {status: 200, data: await new DataBase('cash_transfer').getBy('child_id', data.id)};
                  break;
              case 404:
                  r_data = {status: 404, description: 'no child with such id'};
