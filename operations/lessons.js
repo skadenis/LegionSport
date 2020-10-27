3932734364a25ca7a609c53bbf9a8b600b51200b
Denis <skadenis@mail.ru>
2020-10-28 01:09:03 +0300

    add objects

--- operations/lessons.js
+++ operations/lessons.js
@@ -13,26 +13,25 @@ module.exports = class groups {
 
 
     static async get_all(){
-        return await new DataBase('groups').getBy('is_deleted', false);
+        return await new DataBase('lessons').getBy('is_deleted', false);
     }
     static async get_all_by_group(data){
         return {
             status: 200,
-            data: await new DataBase('groups').query('SELECT * FROM lessons WHERE group_id = $1 and is_deleted = $2 ORDER BY date_time DESC', [data.id, false])
+            data: await new DataBase('lessons').query('SELECT * FROM lessons WHERE group_id = $1 and is_deleted = $2 ORDER BY date_time DESC', [data.id, false])
         };
     }
 
     async get_info(data){
         return {
             status: 200,
-            data: await new DataBase('groups').getById(data.id),
-            childs_in_group:await this.childs_in_group({group_id: data.id})
+            data: await new DataBase('lessons').getById(data.id)
         }
     }
     async create(data){
         // data format
         // {name: 'string', description: 'string'}
-        let Sdata = await new DataBase('groups').add(data);
+        let Sdata = await new DataBase('lessons').add(data);
         let answ =  (await this.get_info({id:Sdata.id})).data;
 
         return{
@@ -43,7 +42,7 @@ module.exports = class groups {
     async edit(data){
         // data format
         // {id: 'int', name: 'string', description: 'string'}
-        let Sdata = await new DataBase('groups').edit(data);
+        let Sdata = await new DataBase('lessons').edit(data);
         return{
             status: 200,
             data: (await this.get_info({id:Sdata.id})).data
@@ -54,9 +53,9 @@ module.exports = class groups {
             id: data.id,
             is_deleted: true
         };
-        await new DataBase('groups').edit(update_data);
+        await new DataBase('lessons').edit(update_data);
         return {
-            status: 200, groups: await new DataBase('groups').DB_query('SELECT * FROM groups WHERE object_id = $1 and is_deleted = $2', [data.id,false])
+            status: 200
         }
     }
 
