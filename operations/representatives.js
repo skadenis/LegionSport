a1963461778fa7a1cb46077a7cd4800eef3c9393
Denis <skadenis@mail.ru>
2020-10-28 00:56:52 +0300

    add objects

--- operations/representatives.js
+++ operations/representatives.js
@@ -10,15 +10,20 @@ module.exports = class representatives {
       return await new DataBase('representative').DB_query('SELECT * FROM representative WHERE child_id = $1 and is_deleted = $2', [data.child_id, false]);
   }
   async add_child_representatives(data){
-      return await new DataBase('representative').add(data);
+      let repr = this.get_child_representatives({child_id: data.child_id});
+      return {
+          status:200,
+          info: repr
+      };
   }
   async edit_representative(data){
       let users = await new DataBase('representative').getBy('id',data.id);
+      let repr = this.get_child_representatives({child_id: users[0].child_id});
 
       if(users.length > 0){
           return {
               status:200,
-              info:await new DataBase('representative').edit(data)
+              info: repr
           };
       }else {
           return {
