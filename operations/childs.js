e4a5452cbf398577d07fb74f35fcaca7260ef268
Denis <skadenis@mail.ru>
2020-10-31 16:32:06 +0300

    add objects

--- operations/childs.js
+++ operations/childs.js
@@ -16,11 +16,11 @@ module.exports = class childs {
     static async get_child_info(data){
         let childsA = await new DataBase('childs').getBy('id', data.id);
         if(childsA.length > 0){
-            if(childsA[0].description == null){
+            if(childsA[0].description){
                 childsA[0].description = '';
             }
 
-            if(childsA[0].email == null){
+            if(childsA[0].email){
                 childsA[0].email = '';
             }
 
