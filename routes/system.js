13b95d4216ffc405251cf24b9d9f4acd2e3568ec
Denis <skadenis@mail.ru>
2020-11-01 17:57:40 +0300

    add objects

--- routes/system.js
+++ routes/system.js
@@ -1,15 +1,17 @@
 'use strict';
 let express = require('express');
 let router = express.Router();
+let jwt = require('jsonwebtoken');
+
+let config = require('../components/config/index');
 
 let Policy = require('cors');
 const verifyToken = require('../components/functions/Token');
 const CheckAuthorization = require('../components/functions/CheckAuthorization');
 
 router.get('/', Policy(), verifyToken, CheckAuthorization, async function(req, res, next) {
-    // Обязательно надо после настроить SchemaJS
-    // Обязательно настроить для учетных записей параоль перевод в хеш md5
-    res.json({status:200});
+    let data = jwt.verify(req.token, config.jwt.secretKey);
+    res.json({status:200, data});
 });
 
 module.exports = router;
