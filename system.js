2946bc3eea87c0a8192cdc9faf3920b584dd0c71
denis <skadenis@mail.ru>
2021-01-02 02:13:46 +0300

    some fix

--- system.js
+++ system.js
@@ -6,5 +6,5 @@ let sys = require('./operations/system');
 setInterval(async function (){
     await new sys.func_payment_for_class()
     },
-    60000*60 // Раз в 1 час
+    60000*15 // Раз в 1 час
 );
