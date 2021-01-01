8043b920c721413a1d8a3bd39d3053a8e24c7be3
denis <skadenis@mail.ru>
2021-01-02 02:50:00 +0300

    some fix

--- operations/childs.js
+++ operations/childs.js
@@ -113,7 +113,8 @@ module.exports = class childs {
             childsA[0].description = childsA[0].description ? childsA[0].description : "";
             childsA[0].email = childsA[0].email ? childsA[0].email : "";
             childsA[0].phone = childsA[0].phone ? childsA[0].phone : "";
-            childsA[0].wallet = Math.round10(childsA[0].wallet, -2);
+            childsA[0].wallet = Math.round(childsA[0].wallet * 100) / 100;
+
 
             return {
                 status: 200,
@@ -227,52 +228,3 @@ Date.prototype.addHours= function(h){
     this.setHours(this.getHours()+h);
     return this;
 }
-
-    // Замыкание
-    (function() {
-        /**
-         * Корректировка округления десятичных дробей.
-         *
-         * @param {String}  type  Тип корректировки.
-         * @param {Number}  value Число.
-         * @param {Integer} exp   Показатель степени (десятичный логарифм основания корректировки).
-         * @returns {Number} Скорректированное значение.
-         */
-        function decimalAdjust(type, value, exp) {
-            // Если степень не определена, либо равна нулю...
-            if (typeof exp === 'undefined' || +exp === 0) {
-                return Math[type](value);
-            }
-            value = +value;
-            exp = +exp;
-            // Если значение не является числом, либо степень не является целым числом...
-            if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-                return NaN;
-            }
-            // Сдвиг разрядов
-            value = value.toString().split('e');
-            value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-            // Обратный сдвиг
-            value = value.toString().split('e');
-            return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-        }
-
-        // Десятичное округление к ближайшему
-        if (!Math.round10) {
-            Math.round10 = function(value, exp) {
-                return decimalAdjust('round', value, exp);
-            };
-        }
-        // Десятичное округление вниз
-        if (!Math.floor10) {
-            Math.floor10 = function(value, exp) {
-                return decimalAdjust('floor', value, exp);
-            };
-        }
-        // Десятичное округление вверх
-        if (!Math.ceil10) {
-            Math.ceil10 = function(value, exp) {
-                return decimalAdjust('ceil', value, exp);
-            };
-        }
-    })();
