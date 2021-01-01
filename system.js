f6d1779cc58889c85bb864ac5bfeb470f0f6699d
denis <skadenis@mail.ru>
2021-01-02 02:31:15 +0300

    some fix

--- system.js
+++ system.js
@@ -5,7 +5,7 @@ let sys = require('./operations/system');
 module.exports = function (){
     setInterval(async function (){
 
-            await new sys.func_payment_for_class()
+            await sys.func_payment_for_class()
         },
         60000 // Раз в 1 час
     );
