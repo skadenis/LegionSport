5a6bd77392c5e475707b36382d7c9ff17ac7df53
denis <skadenis@mail.ru>
2021-01-02 03:44:00 +0300

    some fix

--- operations/system.js
+++ operations/system.js
@@ -142,8 +142,5 @@ Date.prototype.yyyymmdd = function() {
     var mm = this.getMonth() + 1; // getMonth() is zero-based
     var dd = this.getDate();
 
-    return [this.getFullYear(),
-        (mm>9 ? '' : '0') + mm,
-        (dd>9 ? '' : '0') + dd
-    ].join('');
+    return [(dd>9 ? '' : '0') + dd, (mm>9 ? '' : '0') + mm, this.getFullYear()].join('-');
 };
